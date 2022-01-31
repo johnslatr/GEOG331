511cae8a2d125d5d9df7a86ce42259d3f50ec113
johnslatr <jslater@colgate.edu>
2022-01-31 12:10:38 -0500

    Commit 2

--- PracticeScriptActivity2.R
+++ PracticeScriptActivity2.R
@@ -1,4 +1,10 @@
 #Activity 2 Practice 
 
-# make a vector of tree heights 
+# Make a vector of tree heights in meters
 heights <- c(30,41,20,22)
+
+# Convert to cm
+heights_cm <- heights*100
+
+# Set up a matrix 
+Mat <- matrix(c(1,2,3,4,5,5),ncol=2,byrow=TRUE)
