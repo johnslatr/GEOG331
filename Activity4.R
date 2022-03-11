01bdd339d5819ec20b2b9fe8b8210aa29acfb89e
johnslatr <jslater@colgate.edu>
2022-03-11 12:18:08 -0500

    halfway through q3

--- Activity4.R
+++ Activity4.R
@@ -25,13 +25,11 @@ library(ggplot2)
 flower <- iris[iris$Species == "versicolor",]
 firstVec <- list(flower$Sepal.Length, flower$Petal.Length, flower$Sepal.Length)
 secondVec <- list(flower$Sepal.Width, flower$Petal.Width, flower$Petal.Length)
-fit_summ <- list()
+fit <- list()
 
 for (i in 1:3) {
-  fit[i] <- lm(firstVec[i] ~ secondVec[i])
-  fit_summ <- list(fit)
+  fit[i] <- lm(unlist(firstVec[i]) ~ unlist(secondVec[i]))
 }
-
 
 #####################################
 ##### Part 2: data in dplyr     #####
@@ -42,7 +40,7 @@ for (i in 1:3) {
 height <- data.frame(Species = c("virginica","setosa","versicolor"),
                      Height.cm = c(60,100,11.8))
 
-
+joined_height <- full_join(iris, height)
 
 #####################################
 ##### Part 3: plots in ggplot2  #####
@@ -52,10 +50,12 @@ height <- data.frame(Species = c("virginica","setosa","versicolor"),
 plot(iris$Sepal.Length,iris$Sepal.Width)
 
 #3a. now make the same plot in ggplot
-
+ggplot(iris, aes(Sepal.Length, Sepal.Width)) +
+  geom_point() 
 
 #3b. make a scatter plot with ggplot and get rid of  busy grid lines
-
+ggplot(iris, aes(Sepal.Length, Sepal.Width)) +
+  geom_point() + theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank())
 
 #3c. make a scatter plot with ggplot, remove grid lines, add a title and axis labels, 
 #    show species by color, and make the point size proportional to petal length
