6fa1eb1b2f71be7ddaa4f7521df2ff8169050fc0
johnslatr <jslater@colgate.edu>
2022-03-11 18:01:21 -0500

    Completed Activity 4

--- Activity4.R
+++ Activity4.R
@@ -5,6 +5,7 @@ head(iris)
 install.packages(c("dplyr"))
 install.packages(c("ggplot2"))
 
+# Load the libraries and functions
 library(dplyr)
 library(ggplot2)
 
@@ -22,12 +23,19 @@ library(ggplot2)
 
 # hint: consider using a list, and also new vectors for regression variables
 
+# Store our flower that we're looking at 
 flower <- iris[iris$Species == "versicolor",]
+
+#Define two vectors that list the variables that will allow us to iterate through what we want between them for the question (like pairs)
 firstVec <- list(flower$Sepal.Length, flower$Petal.Length, flower$Sepal.Length)
 secondVec <- list(flower$Sepal.Width, flower$Petal.Width, flower$Petal.Length)
+
+# create an empty list for our result to go into
 fit <- list()
 
+#create for loop to iterate through the list, doing regressions on the way between proposed variables 
 for (i in 1:3) {
+  # Put results in our list 
   fit[i] <- lm(unlist(firstVec[i]) ~ unlist(secondVec[i]))
 }
 
@@ -37,9 +45,11 @@ for (i in 1:3) {
 
 #use dplyr to join data of maximum height
 #to a new iris data frame
+# define height variable 
 height <- data.frame(Species = c("virginica","setosa","versicolor"),
                      Height.cm = c(60,100,11.8))
 
+# join the two data frames
 joined_height <- full_join(iris, height)
 
 #####################################
@@ -50,15 +60,17 @@ joined_height <- full_join(iris, height)
 plot(iris$Sepal.Length,iris$Sepal.Width)
 
 #3a. now make the same plot in ggplot
-ggplot(iris, aes(Sepal.Length, Sepal.Width)) +
+plot <- ggplot(iris, aes(Sepal.Length, Sepal.Width)) +
   geom_point() 
 
 #3b. make a scatter plot with ggplot and get rid of  busy grid lines
-ggplot(iris, aes(Sepal.Length, Sepal.Width)) +
-  geom_point() + theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank())
+plotNoGrid <- plot + theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank())
 
 #3c. make a scatter plot with ggplot, remove grid lines, add a title and axis labels, 
 #    show species by color, and make the point size proportional to petal length
+
+cplot <- ggplot(iris, aes(Sepal.Length, Sepal.Width, color = Species )) +
+  geom_point(size = iris$Petal.Length)  + labs(title = "Sepal Length vs Width for Iris Species", x = "Sepal Length", y = "Sepal Width") + theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank())
 
 #####################################
 ##### Question: how did         #####
