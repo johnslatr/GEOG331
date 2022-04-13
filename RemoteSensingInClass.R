c1dc8989103a5078792e29a34f5c063aec0ecd6f
johnslatr <jslater@colgate.edu>
2022-04-13 12:00:25 -0400

    Updated

--- RemoteSensingInClass.R
+++ RemoteSensingInClass.R
@@ -19,3 +19,21 @@ plotRGB(p, r=3, g=2, b=1)
 plotRGB(p, r=3, g=2, b=1, 
         scale = 65535, # to fix color intensity problem
         stretch = "hist") #for contrast because values for RGB are closer to zero 
+
+# read file with field observations of canopy cover 
+tree <- read.csv("Z:/students/jslater/Data/rs_data/siberia_stand_data.csv", header= T)
+
+#convert to vector object using terra package 
+gtree <- vect(tree, geom = c("Long", "Lat"), "epsg:4326")
+
+# project the data to match the coordinate system of the raster layer 
+gtree2 <- project(gtree, p)
+
+#create a polygon from the extent of the points 
+b <- as.lines(ext(gtree), "epsg:4326")
+
+# reproject the polygons to the same projection as our raster 
+b2 <- project(b,crs(p))
+
+# buffer the extent by 200m
+b3 <- buffer(b2, width = 200)
