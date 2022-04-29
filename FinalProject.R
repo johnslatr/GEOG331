900291a7f7680b8a5f9b4c5fa5303f8f4ea9c48b
johnslatr <jslater@colgate.edu>
2022-04-29 12:10:17 -0400

    temp data read in

---
+++ FinalProject.R
@@ -0,0 +1,80 @@
+# install -- Only do once!
+#install.packages('R.utils')
+install.packages("BiocManager")
+install.packages("neonUtilities")
+
+# Libraries
+library(data.table)
+library("rhdf5")
+library(neonUtilities)
+
+# set working directory
+setwd("Z:/students/jslater/")
+
+
+# saving file paths
+f <- "Z:/GEOG331_S22/students/jslater/Data/NEON_eddy-flux/NEON.D03.DSNY.DP4.00200.001.2017-03.basic.20220120T173946Z.RELEASE-2022/NEON.D03.DSNY.DP4.00200.001.nsae.2017-03.basic.20211220T152138Z.h5/NEON.D03.DSNY.DP4.00200.001.nsae.2017-03.basic.20211220T152138Z.h5"
+fileUrl <- "Z:/students/jslater/Data/NEON_eddy-flux/NEON.D03.DSNY.DP4.00200.001.2017-03.basic.20220120T173946Z.RELEASE-2022/NEON.D03.DSNY.DP4.00200.001.nsae.2017-03.basic.20211220T152138Z.h5/NEON.D03.DSNY.DP4.00200.001.nsae.2017-03.basic.20211220T152138Z.h5"
+fileUrl2 <- "Z:/students/jslater/Data/NEON_eddy-flux/NEON.D03.DSNY.DP4.00200.001.2020-07.basic.20220120T173946Z.RELEASE-2022/NEON.D03.DSNY.DP4.00200.001.nsae.2020-07.basic.20211220T175101Z.h5"
+
+
+
+# Read in data to flux variable
+# MARCH 2017
+flux <- stackEddy(filepath = fileUrl,
+                  level = "dp04")
+# JULY 2020
+flux2 <- stackEddy(filepath = fileUrl2,
+                   level = "dp04")
+
+# Plot Graph
+p <- plot(flux2$DSNY$data.fluxCo2.nsae.flux ~ flux$DSNY$timeBgn, 
+     type="l", pch=".", xlab="Time", ylab="CO2 flux")
+
+integrate(p)
+
+plot(flux2$DSNY$data.fluxCo2.stor.flux ~ flux2$DSNY$timeBgn,
+     type="l", pch=".", xlab="Time", ylab="CO2 Storage")
+
+
+# Unzip files 
+unzip(zipfile, files = NULL, list = FALSE, overwrite = TRUE,
+      junkpaths = FALSE, exdir = ".", unzip = "internal",
+      setTimes = FALSE)
+
+# Read in temperature files
+
+tdata <- read.csv("Z:/students/jslater/Data/NEON_temp-air-triple/NEON.D03.DSNY.DP1.00003.001.2017-01.basic.20220120T173946Z.RELEASE-2022/NEON.D03.DSNY.DP1.00003.001.000.040.030.TAAT_30min.2017-01.basic.20211210T223026Z.csv")
+
+
+plot(tdata$startDateTime ~ tdata$tempTripleMean, 
+     type="p", pch=".", xlab="Date", ylab="Temperature", ylim = c(-20,40))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# list.files will list everything in working directory, recursive = true, simple pattern matching, fullpath = true gives full path
+# for loop to unzip all files, list all hdf5 files, write another loop to read NSAE variables and length of it 
+
+# Unzip .gz file
+gunzip(fileUrl)
+
+# read in data
+h5readAttributes(file = fileURL,
+                 name = "DSNY/dp01/data")
